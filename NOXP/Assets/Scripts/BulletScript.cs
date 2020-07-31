14e7cd10457a88ce4ce2e459cefd0aebc9aab9ac
Sam <samcpollock@gmail.com>
2020-07-31 09:07:27 -0400

    Added the project to the repo
    this is halfway through the second tutorial vid

---
+++ NOXP/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    float maxDistanceToMove = speed * Time.deltaTime;
+        transform.position += transform.forward * maxDistanceToMove;
+        if (transform.position.z > 15)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ NOXP/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public GameObject playerBulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    float maxDistanceToMove = speed * Time.deltaTime;
+
+            transform.position += Vector3.forward * Input.GetAxis("Vertical") * maxDistanceToMove;
+            transform.position += Vector3.right * Input.GetAxis("Horizontal") *maxDistanceToMove;
+    if (Input.GetMouseButton(0))
+        {
+            Instantiate(playerBulletPrefab, transform.position, transform.rotation);
+        }
+    }
+}
